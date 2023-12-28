5c018dba2c650ad17e94545247e4220cc2e5afe1
Chris Carbonell <ccarbonell228@gmail.com>
2023-12-28 15:00:07 -0600

    feature: postgres db

---
+++ db/sql/init/create_simulations.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS simulations (
+    simulation_id SERIAL PRIMARY KEY,
+    dt_entered TIMESTAMP WITH TIME ZONE,
+    player VARCHAR(255),
+    is_playable BOOLEAN,
+    player_won BOOLEAN,
+    turns SMALLINT,
+    lives SMALLINT,
+    len_deck SMALLINT,
+    state_initial JSON,
+    state_end JSON
+);
